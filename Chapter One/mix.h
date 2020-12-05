c63aa9e262f6b780283a7790bbc98c351991cb4b
Kevin Colour <kecolour@gmail.com>
2020-12-04 22:57:53 -0800

    Operators

---
+++ Chapter One/mix.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+ * FILENAME:    mix_.h
+ * DESCRIPTION:
+ *     Header for MIX Files.
+ * ENVIRONMENT:
+ *     macOS Cataline 10.15.7
+ *     Visual Studio Code 1.30
+ * AUTHORS:
+ *     Kevin Colour
+ * DATES:
+ *     Created: 04Dec2020
+***************************************************************************/
+
+#ifndef MIX_H
+# define MIX_H
+
+#include <stdio.h>
+
+// https://mcdlr.com/utf-8/#64964
+
+#define S(str) =
+#define ︌︍(str) S(str)
+
+#endif
